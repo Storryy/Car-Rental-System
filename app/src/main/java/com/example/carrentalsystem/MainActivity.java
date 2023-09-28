1abad77f48828f80d50863855fb705a891ac984a
Storryy <pranav.dhadwal@gmail.com>
2023-09-28 18:22:31 +0530

    Version 1.0(Including Login and Register

--- app/src/main/java/com/example/carrentalsystem/MainActivity.java
+++ app/src/main/java/com/example/carrentalsystem/MainActivity.java
@@ -14,6 +14,7 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
 
     private Button Logout_Button;
+    private Button Hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
